e39b567abe727649326d3c5af701fc569313a8f6
brett-fisher <bdf1221@gmail.com>
2020-05-02 19:29:55 -0400

    Added models

---
+++ InvestmentAPI/InvestmentAPI/Models/Investment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvestmentAPI.Models
+{
+    public class Investment
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime TimeOfPurchase { get; set; }
+    }
+}
---
+++ InvestmentAPI/InvestmentAPI/Models/InvestmentDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvestmentAPI.Models
+{
+    public class InvestmentDetail
+    {
+        [Key]
+        public int SystemId { get; set; }
+        public int InvestmentId { get; set; }
+        public int Shares { get; set; }
+        public double CostBasisPerShare { get; set; }
+        public double CurrentPrice { get; set; }
+        public double CurrentValue { get; set; }
+        public byte Term { get; set; }
+        public double NetValuation { get; set; }
+    }
+}
+
