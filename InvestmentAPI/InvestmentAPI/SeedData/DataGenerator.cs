c86f1d24912b36d47b709298634d0c3790730a18
brett-fisher <bdf1221@gmail.com>
2020-05-02 21:14:41 -0400

    Added some generated data

--- InvestmentAPI/InvestmentAPI/SeedData/DataGenerator.cs
+++ InvestmentAPI/InvestmentAPI/SeedData/DataGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using InvestmentAPI.Contexts;
+using InvestmentAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,6 +14,8 @@ namespace InvestmentAPI.SeedData
             using (var context = new InvestmentContext(
                 service.GetRequiredService<DbContextOptions<InvestmentContext>>()))
             {
+                Random rand = new Random();
+
                 if (context.Investments.Any())
                 {
                     return;
@@ -23,7 +26,27 @@ namespace InvestmentAPI.SeedData
                     return;
                 }
 
-                
+                var inv1 = new Investment()
+                {
+                    Id = 1,
+                    UserId = 100,
+                    Name = "APPL",
+                    TimeOfPurchase = DateTime.Now
+                };
+
+                var inv1Detail = new InvestmentDetail()
+                {
+                    SystemId = 100,
+                    InvestmentId = 1,
+                    Shares = rand.Next(1, 20),
+                    CostBasisPerShare = 297.31,
+                    CurrentPrice = 0,
+                    CurrentValue = 0,
+                    Term = 0,
+                    NetValuation = 0
+                };
+
+
             }
         }
     }
