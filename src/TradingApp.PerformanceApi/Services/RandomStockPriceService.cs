3875e5ebd820cf5fadfc549b7a1066996a70c35d
Tim Rice <timrice+timrtc@gmail.com>
2020-05-21 22:31:52 -0400

    feat(services): get randomized current stock pricing

---
+++ src/TradingApp.PerformanceApi/Services/RandomStockPriceService.cs
@@ -0,0 +1,16 @@
+﻿namespace TradingApp.PerformanceApi.Services
+{
+    using Bogus;
+
+    /// <summary>
+    /// An implementation of the <see cref="IStockPriceService"/> that returns randomized prices.
+    /// </summary>
+    public class RandomStockPriceService : IStockPriceService
+    {
+        /// <inheritdoc />
+        public decimal GetCurrentPrice(string name)
+        {
+            return new Faker().Finance.Amount();
+        }
+    }
+}
---
+++ test/TradingApp.PerformanceApi.Tests/Services/RandomStockPriceServiceTests.cs
@@ -0,0 +1,40 @@
+﻿namespace TradingApp.PerformanceApi.Tests.Services
+{
+    using TradingApp.PerformanceApi.Services;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// Unit tests for the <see cref="RandomStockPriceService"/> class.
+    /// </summary>
+    public class RandomStockPriceServiceTests
+    {
+        private readonly ITestOutputHelper output;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomStockPriceServiceTests"/> class.
+        /// </summary>
+        /// <param name="output">Test logger.</param>
+        public RandomStockPriceServiceTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        /// <summary>
+        /// It should return a randomized current price.
+        /// </summary>
+        [Fact]
+        public void ShouldReturnRandomizedPrice()
+        {
+            // arrange
+            var sut = new RandomStockPriceService();
+
+            // act
+            var price = sut.GetCurrentPrice("FOO");
+            this.output.WriteLine($"Randomized price: {price}");
+
+            // assert
+            Assert.True(price >= 0);
+        }
+    }
+}
