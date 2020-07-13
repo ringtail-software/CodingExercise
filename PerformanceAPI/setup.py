5211aa4e6647e0cd1f5c3c5bcbe96453f33c294a
Nolan Prescott <mail@nprescott.com>
2020-07-13 00:24:49 -0400

    include plugin in configured path
    necessary during installation via `pip`

--- PerformanceAPI/setup.py
+++ PerformanceAPI/setup.py
@@ -5,7 +5,7 @@ setup (
     version          = "1.0",
     description      = "Investment web performance API.",
     package_dir      = {"": "src"},
-    packages         = find_packages("src"),
+    packages         = find_packages("src") + ["twisted.plugins"],
     install_requires = ["twisted>=20.3.0",
                         "klein>=20.6.0",
                         "treq>=20.4.1"]    
