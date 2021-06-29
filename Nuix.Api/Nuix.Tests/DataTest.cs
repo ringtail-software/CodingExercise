7322d73c2ad6feef9cd1d3f70a3dfd0e5c22bf14
Mark <owen.c.mark@gmail.com>
2021-06-29 09:09:52 -0400

    Cleanup.

--- Nuix.Api/Nuix.Tests/DataTest.cs
+++ Nuix.Api/Nuix.Tests/DataTest.cs
@@ -5,46 +5,59 @@ using Xunit;
 
 namespace Nuix.Tests
 {
+    // Arrange! Act! Assert!
+
     public class DataTest
     {
+        public DataTest()
+        {
+            _sut = new Repository();
+        }
+
+        [Fact(Skip =  "Just playing.")]
+        [Trait("Category", "Ignore")]
+        public async void HereBeDragoons()
+        {
+            Assert.True(false);
+        }
+
         [Fact]
+        [Trait("Category", "Data")]
         public async void HasData()
         {
-            var sut = new Repository();
-
-            var result = await sut.GetUserInvestments("Mark");
+            var result = await _sut.GetUserInvestments("Mark");
 
             Assert.True(result.Any());
         }
 
         [Fact]
+        [Trait("Category", "Data")]
         public async void IsCaseInsensitive()
         {
-            var sut = new Repository();
-
-            var result = await sut.GetUserInvestments("mArK");
+            var result = await _sut.GetUserInvestments("mArK");
 
             Assert.True(result.Any());
         }
 
         [Fact]
+        [Trait("Category", "Data")]
         public async void HasNoData()
         {
-            var sut = new Repository();
-
-            var result = await sut.GetUserInvestments("jim");
+            var result = await _sut.GetUserInvestments("jim");
 
             Assert.False(result.Any());
         }
 
         [Fact]
+        [Trait("Category", "Data")]
         public async void HasMoreThanOneInvestment()
         {
-            var sut = new Repository();
-
-            var result = await sut.GetUserInvestments("mark");
+            var result = await _sut.GetUserInvestments("mark");
 
             Assert.True(result.Count() > 1);
         }
+
+        // System under test.
+        private readonly IRepository _sut;
     }
 }
